e77b3594a17221955cd0da422d6c915efab67aae
eyabc <bey4314@naver.com>
2020-06-04 14:17:46 +0900

    feat: data init

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,21 +1,48 @@
+import { validateAllocatedOrders } from './utils/validator.js';
+
 export default class Dashboard {
-  $target = null
-  $driverTable = null
-  $driverTableSortInfo = null
-  $driverInfoModal = null
+  $target = null;
+  $driverTable = null;
+  $driverTableSortInfo = null;
+  $driverInfoModal = null;
 
   data = {
     drivers: [],
     orders: [],
     places: [],
-  }
-  driverData = []
+  };
+  driverData = [];
 
-  setState(data) {
-    this.data = data
-    this.render()
+
+  aggregateDriverData () {
+    const aggregateDriverData = [];
+
+    this.data.drivers.forEach(driver => {
+      const driverInfo = {
+        id: driver.id,
+        name: driver.name,
+        firstOrderedAt: '',
+        todayDeliveryMenuPrice: 0,
+        todayDeliveryDistance: 0,
+        reservedOrders: [],
+        orders: [],
+      };
+      const driverOrders = this.data.orders
+        .filter(order => order.driverId === driver.id);
+      driverInfo.orders = driverOrders;
+
+      aggregateDriverData.push(driverInfo);
+    });
+    this.driverData = aggregateDriverData;
   }
-  render() {
+
+  setState (data) {
+    this.data = data;
+    this.aggregateDriverData();
+    this.render();
+  }
+
+  render () {
 
   }
 }
