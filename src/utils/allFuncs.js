aa552006760e12c87715dda9c446e80bbb827e7b
eyabc <bey4314@naver.com>
2020-06-17 23:37:29 +0900

    feat: 요구사항 2.1 배차하기 배차전 driverOrders 아직 남은 일정을 채움

--- src/utils/allFuncs.js
+++ src/utils/allFuncs.js
@@ -1,10 +1,20 @@
 const getMillisecondTime = (timeArr) => {
+  const [Y, M, D] = getNowDate();
+  return timeArr.map(time => +new Date(`${ Y }-${ M }-${ D } ${ time }`));
+};
+
+const getNowDate = () => {
   const Y = new Date().getFullYear();
   const M = new Date().getMonth() + 1;
   const D = new Date().getDate();
-
-  return timeArr.map(time => +new Date(`${ Y }-${ M }-${ D } ${ time }`));
+  return [Y, M, D];
 };
+
+const getDateTime = (timeArr) => {
+  const [Y, M, D] = getNowDate();
+  return timeArr.map(time => new Date(`${ Y }-${ M }-${ D } ${ time }`));
+}
+
 
 const booleanValidPrice = (orderedAt, deliveredAt) => {
   // 1000ms(1초) * 60(1분) * 60(1시간)
@@ -13,12 +23,13 @@ const booleanValidPrice = (orderedAt, deliveredAt) => {
 };
 
 const getDeliveryMenuPrice = (driverOrders) => {
-  return driverOrders.reduce((sum, order) => {
-    if (order.deliveredAt.length === 0) return sum;
+  return driverOrders.reduce(([valid, inValid], order) => {
+    if (order.deliveredAt.length === 0) return [valid, inValid];
+
     const [orderedAt, deliveredAt] = getMillisecondTime([order.orderedAt, order.deliveredAt]);
     const check = booleanValidPrice(orderedAt, deliveredAt);
-    return sum + (check * order.price);
-  }, 0);
+    return [valid + (check * order.price), inValid + (!check * order.price)];
+  }, [0, 0]);
 };
 
 const getPlaceById = (places, placeId) => places.find(place => place.id === placeId);
@@ -82,9 +93,44 @@ const setLSSortBase = (newField) => {
     { field, isDescending: !isDescending }
     : { field: newField, isDescending: false };
 
-  localStorage.setItem('sortBase',JSON.stringify(res));
+  localStorage.setItem('sortBase', JSON.stringify(res));
   return res;
 };
 
+const fillExpectDelivery = (driverOrders, reservedOrders) => {
+  const driverOrdersLen = driverOrders.length;
+  let lastDeliveryPos, lastDeliveryTime, placePos, deliveryPos, orderTime;
+  let baseTime;
 
-export { getDeliveryMenuPrice, getDeliveryDistance, sortByField, getLSSortBase, setLSSortBase };
+  if (driverOrders.length) {
+    const lastOrder = driverOrders[driverOrdersLen - 2],
+      thisOrder = driverOrders[driverOrdersLen - 1];
+
+    if (thisOrder.pickedUpAt === '') {
+      lastDeliveryPos = lastOrder.deliveryPosition;
+      placePos = thisOrder.placePosition;
+      deliveryPos = thisOrder.deliveryPosition;
+      const toPlaceDist = getDist(lastDeliveryPos, placePos);
+      const toDeliveryDist = getDist(placePos, deliveryPos);
+
+      [lastDeliveryTime, orderTime] = [lastOrder.deliveredAt, thisOrder.orderedAt];
+      const [MlastDeliveryTime, MorderTime] = getMillisecondTime([lastDeliveryTime, orderTime]);
+      const [DlastDeliveryTime, DorderTime] = getDateTime([lastDeliveryTime, orderTime]);
+      baseTime = MlastDeliveryTime > MorderTime ? DlastDeliveryTime : DorderTime;
+      const pickedUpAt = new Date(new Date(baseTime).setMinutes(baseTime.getMinutes()+toPlaceDist));
+      const deliveredAt = new Date(new Date(pickedUpAt).setMinutes(pickedUpAt.getMinutes()+ toDeliveryDist));
+      thisOrder.pickedUpAt = `${pickedUpAt.getHours()}:${pickedUpAt.getMinutes()}`;
+      thisOrder.deliveredAt = `${deliveredAt.getHours()}:${deliveredAt.getMinutes()}`;
+    }
+  }
+};
+
+export {
+  getDeliveryMenuPrice,
+  getDeliveryDistance,
+  sortByField,
+  getLSSortBase,
+  setLSSortBase,
+  fillExpectDelivery,
+  getPlaceById,
+};
